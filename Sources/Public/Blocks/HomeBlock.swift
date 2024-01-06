918aeb94d3866301f60da86fb2a22930def017ee
zane <39070793+zaneenders@users.noreply.github.com>
2024-01-05 20:52:18 -0700

    nested block

--- Sources/Public/Blocks/HomeBlock.swift
+++ Sources/Public/Blocks/HomeBlock.swift
@@ -1,9 +1,20 @@
 import WebsiteBuilder
 
+// Needs to be public so the server can setup from there
 public struct HomeBlock: Block {
+
     public init() {}
+
     public var component: some Block {
+        Button("Button")
         Text("Zane Was here")
         Text("Welcome to the rabbit hole")
+        Nested()
+    }
+}
+
+struct Nested: Block {
+    var component: some Block {
+        Text("Nested")
     }
 }

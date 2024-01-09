9b163e3a1980550174f72cdedcfe59759789b130
zane <39070793+zaneenders@users.noreply.github.com>
2024-01-09 08:26:20 -0700

    init Scribe manifesto/vision

---
+++ Sources/Public/ScribeManifesto.swift
@@ -0,0 +1,13 @@
+/*
+# Scribe
+
+a new OS
+- start as a web framework to leverage the webbroswer
+- embeed the broswer in the OS
+    - there are roughly 3 ways to draw on teh screen
+        - game engine 
+        - web broswer
+        - OS primitives
+    SO why not embeded one or all of them into the OS
+*/
+

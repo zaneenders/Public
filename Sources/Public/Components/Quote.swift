f32e88882479ba6c539ba6a98c3542a2f854375e
zane <39070793+zaneenders@users.noreply.github.com>
2024-06-05 22:43:12 -0600

    Added Foreground color.

--- Sources/Public/Components/Quote.swift
+++ Sources/Public/Components/Quote.swift
@@ -6,7 +6,7 @@ struct Quote: Block {
     let who: String
     var component: some Block {
         Group(.horizontal) {
-            Text(tile)
+            Text(tile).style([ForegroundColor(.blue)])
             Text(":")
             Text(quote)
             Text("-")
